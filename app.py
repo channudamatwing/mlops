3daabb52d6d77a8714cf172ccb2e7966c49d9680
channudamatwing <channudam.ray@wingbank.com>
2024-05-07 03:01:29 +0000

    updated code

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def homepage():
-    return render_template("index.html",  **{"greeting": "Hello from Flask!"})
+    return render_template("index.html")
+
+@app.route("/users", methods=['GET'])
+def getUsers():
+    return jsonify (
+        {'name': 'alice','email': 'alice@outlook.com'}
+    )
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
--- static/index.js
+++ static/index.js
@@ -4,13 +4,23 @@ const vm = new Vue({
     data(){
         return{
             message: "Hello from VueJs in Flask",
-            counter: 0
+            counter: 0,
+            userInfo: undefined
         }
+    },
+    mounted(){
+        axios.get("https://psychic-meme-4jqxq6jr6w6qfv45-5000.app.github.dev/users",{
+            headers:{
+                'Content-Type': 'application/json',
+            }
+        }).then(data=>{
+           this.userInfo = data.data;
+        })
     },
     methods: {
         increaseCounts() {
             this.counter++;
             console.log(`counter: ${this.counter}`)
         }
-    }
+    },
 })
